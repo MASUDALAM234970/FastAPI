301c2d338c752bb86cbfe5db57aebed727710528
Masud Alam <mdmasudalam2349.com>
2026-07-05 13:17:52 +0600

    14

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
-app = FastAPI()
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+app =FastAPI()
 
 
-@app.get("/")
-async  def root ():
-    return{"message" : "Hello world"}
+while True:
+   try: 
+       conn = psycopg2.connect(host='localhost',database="alam",user="postgres",password='234970', cursor_factory=RealDictCursor)
+       cursor=conn.cursor()
+       print("Successfully connected Database")
+       break
+   except Exception as error:
+       print("Database connection failed ")
+       print("Error:",error) 
+       time.sleep(2)
+
+
+@app.get("/api/")
+def alam():
+    cursor.execute("SELECT * FROM course")
+    data = cursor.fetchall()
+    return {"data": data}
